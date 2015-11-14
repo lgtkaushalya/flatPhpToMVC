b277b50cba77325c9e0a76f8d2c75e06763ac2d2
Thilanka <lgtkaushalya@gmail.com>
2015-11-14 13:59:48 +0530

    Flat PHP blog list

---
+++ blog_db.sql
@@ -0,0 +1,25 @@
+--
+-- Database: `blog_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `title`) VALUES
+(1, 'Introduction to Symfony'),
+(2, 'Symfony internals'),
+(3, 'Flat PHP vs Symfony'),
+(4, 'Symfony Routing');
