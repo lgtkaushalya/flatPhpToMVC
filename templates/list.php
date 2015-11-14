94f5c89580106556da3f5df2728f5b8a316bfe4d
Thilanka <lgtkaushalya@gmail.com>
2015-11-14 15:16:24 +0530

    Improved flat php to MVC

---
+++ templates/list.php
@@ -0,0 +1,17 @@
+<?php $title = 'List of Posts' ?>
+<?php ob_start() ?>
+
+  <h1>List of Posts</h1>
+    <ul>
+      <?php foreach($posts as $post) { ?>
+        <li>
+          <a href="/show.php?id=<?php echo $post['id'] ?>">
+            <?php echo $post['title'] ?>
+          </a>
+        </li>
+      <?php } ?>
+    </ul>
+
+<?php $content = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
