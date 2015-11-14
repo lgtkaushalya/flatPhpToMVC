0bf692d57bbb866327ede5cac917dd6144be1c1a
Thilanka <lgtkaushalya@gmail.com>
2015-11-14 15:41:08 +0530

    Added front controller

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
-  require 'model.php';
+// index.php
+// load and initialize any global libraries
+require_once 'model.php';
+require_once 'controllers.php';
 
-  $posts = get_all_posts();
+// route the request internally
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-  require 'templates/list.php';
+if(strpos($uri, '/index.php/list') !== false) {
+  list_action();
+} elseif (strpos($uri, '/index.php/show') !== false && isset($_GET['id'])) {
+  show_action($_GET['id']);
+} else {
+  header('Status: 404 Not Found');
+  echo '<html><body><h1>Page Not Found</h1></body></html>';
+}
